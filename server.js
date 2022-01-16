075643aab91d0e313d664f4f8181a6ed978411fd
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-16 08:54:27 -1000

    Add queries to read, delete, and create a candidate

--- server.js
+++ server.js
@@ -19,6 +19,34 @@ const db = mysql.createConnection(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 11, (err, result) => {
+//   if (err) console.log(err);
+//   console.log(result);
+// });
+
+// CREATE a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) console.log(err);
+  console.log(result);
+});
+
 app.use((req, res) => {
   res.status(404).end();
 });
