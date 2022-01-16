96514d4dcf48a0ef7f1b14b6f59f23647e7edecb
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-16 08:42:56 -1000

    Set up database connection

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mysql = require("mysql2");
 
+// Middleware
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Database connection
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "N0fish!!",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
