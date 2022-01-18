246a5c9accc2d87d5ca37603ad4e5d5fb27e6335
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-17 14:36:40 -1000

    Fix syntax error

--- db/schema.sql
+++ db/schema.sql
@@ -35,5 +35,5 @@ CREATE TABLE votes (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT uc_voter UNIQUE (voter_id),
     CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE,
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
